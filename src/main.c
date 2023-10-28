43a8920e9b2ed297d9c0faf98c89896754f82a01
panos <panos99sk@gmail.com>
2023-10-28 15:30:32 +0300

    switch to c, display

--- src/main.c
+++ src/main.c
@@ -3,7 +3,6 @@
 #include "stdlib.h"
 #include "stdio.h"
 #include "util.h"
-#include <vector>
 #include "math.h"
 
 #define SCRWIDTH 1200
@@ -12,7 +11,7 @@
 
 //extra structs
 typedef struct{
-    float xEP, xNL, u, a, TF, KE, UE;
+    float xEP, xNL, u, a, TF, KE, UE, TE;
 } physicsVals;
 typedef struct{
     float A, K, m, t;
@@ -61,9 +60,14 @@ void draw(){
         //dynamics
         DrawFPS(0, 0);
         physicsVals osc = OscilatorSimulation(simState.A, simState.K, simState.m, "V");
-        DrawCircle(SCRWIDTH/2, osc.xEP, 10, BLACK); //oscillator
-        DrawLineEx((Vector2){SCRWIDTH/2, SCRHEIGHT/2}, (Vector2){SCRWIDTH/2 + simState.A * cos(sqrt(simState.K / simState.m) * simState.t), osc.xEP}, 5.0, BLACK); //trig circle
-        DrawDottedLines(SCRWIDTH/2, osc.xEP, (SCRWIDTH/2 + simState.A * cos(sqrt(simState.K / simState.m) * simState.t)), osc.xEP, BLACK);
+        DrawCircle(SCRWIDTH/2, SCRHEIGHT/2 - osc.xEP, 10, BLACK); //oscillator
+        DrawLineEx((Vector2){SCRWIDTH/2, SCRHEIGHT/2}, (Vector2){SCRWIDTH/2 + simState.A * cos(sqrt(simState.K / simState.m) * simState.t), SCRHEIGHT/2 - osc.xEP}, 5.0, BLACK); //trig circle
+        DrawDottedLines(SCRWIDTH/2, SCRHEIGHT/2 - osc.xEP, (SCRWIDTH/2 + simState.A * cos(sqrt(simState.K / simState.m) * simState.t)), SCRHEIGHT/2 - osc.xEP, BLACK);
+
+        char* values;
+        asprintf(&values, "xEP: %f m \nxNL: %f m\nu: %f m/s \na: %f m/s^2 \nTF: %f N\nKE: %f J\nUE: %f J \nTE: %fJ ", osc.xEP, osc.xNL, osc.u, osc.a, osc.TF, osc.KE, osc.UE, osc.TE);
+        DrawText(values, 5, SCRHEIGHT - 18 * 8 * 3/2, 18, BLACK);
+        free(values);
 
 
     EndDrawing();
@@ -71,11 +75,22 @@ void draw(){
 
 physicsVals OscilatorSimulation(float A, float K, float m, char *surfaceType){
     float omega = sqrt(K/m);
+    float d;
     if (surfaceType == "V"){
-        float d = m*10/K;
+        d = m*10/K;
     }
-    float xEP = SCRHEIGHT/2 - (A * sin(omega * simState.t));
-    return (physicsVals){xEP, xEP, xEP, xEP, xEP, xEP, xEP};
+    float xEP = (A * sin(omega * simState.t));
+    float xNL = (d + xEP);
+    float u = omega * A * cos(omega * simState.t);
+    float a = -omega * omega * A * sin(omega * simState.t);
+    float TF = -K * xEP;
+    float KE = 1/2 * m * u*u;
+    float UE = 1/2 * K * xEP*xEP;
+    float TE = 1/2 * K * A*A;
+
+
+    // float xEP, xNL, u, a, TF, KE, UE;
+    return (physicsVals){xEP: xEP,xNL: xNL,u: u,a: a,TF: TF,KE: KE,UE: UE,TE: TE};
 }
 
 
