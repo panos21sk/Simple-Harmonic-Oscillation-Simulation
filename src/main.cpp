3c6c6757a37bca7776f8d8be4987a995a5e8c3e6
panos <panos99sk@gmail.com>
2023-10-28 13:41:55 +0300

    small rework

--- src/main.cpp
+++ src/main.cpp
@@ -22,39 +22,10 @@ typedef struct{
 //extra functions
 void DrawDottedLines(int xi, int yi, int xf, int yf, Color color);
 physicsVals OscilatorSimulation(float A, float K, float m, char *surfaceType);
+void draw();
 
 
 state simState = (state){A: 300, K: 10, m: 1, t: 0, paused: false, surfaceType: "V"};
-
-
-void draw(){
-    //draw
-    BeginDrawing();
-        //statics
-        DrawRing((Vector2){SCRWIDTH/2, SCRHEIGHT/2}, SCRWIDTH/4, SCRWIDTH/4 + 5, 0, 360, 1, RED);
-        DrawDottedLines(SCRWIDTH/2 - SCRWIDTH/4, SCRHEIGHT/2, SCRWIDTH/2 + SCRWIDTH/4, SCRHEIGHT/2, RED);
-        DrawDottedLines(SCRWIDTH/2, SCRHEIGHT/2 + SCRWIDTH/4, SCRWIDTH/2, SCRHEIGHT/2 - SCRWIDTH/4, BLUE);
-        DrawText("Equilibrium position", SCRWIDTH/2 + SCRWIDTH/4 + 10, SCRHEIGHT/2, 18, RED);
-        ClearBackground(RAYWHITE);
-
-        //dynamics
-        DrawFPS(0, 0);
-        physicsVals osc = OscilatorSimulation(simState.A, simState.K, simState.m, "V");
-        DrawCircle(SCRWIDTH/2, osc.xEP, 10, BLACK); //oscillator
-        DrawLineEx((Vector2){SCRWIDTH/2, SCRHEIGHT/2}, (Vector2){SCRWIDTH/2 + simState.A * cos(sqrt(simState.K / simState.m) * simState.t), osc.xEP}, 5.0, BLACK); 
-
-
-    EndDrawing();
-}
-
-physicsVals OscilatorSimulation(float A, float K, float m, char *surfaceType){
-    float omega = sqrt(K/m);
-    if (surfaceType == "V"){
-        float d = m*10/K;
-    }
-    float xEP = SCRHEIGHT/2 - (A * sin(omega * simState.t));
-    return (physicsVals){xEP, xEP, xEP, xEP, xEP, xEP, xEP};
-}
 
 int main(void)
 {   // initialization
@@ -77,6 +48,35 @@ int main(void)
     return 0;
 }
 
+void draw(){
+    //draw
+    BeginDrawing();
+        //statics
+        DrawRing((Vector2){SCRWIDTH/2, SCRHEIGHT/2}, simState.A, simState.A + 5, 0, 360, 1, RED);
+        DrawDottedLines(SCRWIDTH/2 - simState.A, SCRHEIGHT/2, SCRWIDTH/2 + simState.A, SCRHEIGHT/2, RED);
+        DrawDottedLines(SCRWIDTH/2, SCRHEIGHT/2 + simState.A, SCRWIDTH/2, SCRHEIGHT/2 - simState.A, BLUE);
+        DrawText("Equilibrium position", SCRWIDTH/2 + simState.A + 10, SCRHEIGHT/2, 18, RED);
+        ClearBackground(RAYWHITE);
+
+        //dynamics
+        DrawFPS(0, 0);
+        physicsVals osc = OscilatorSimulation(simState.A, simState.K, simState.m, "V");
+        DrawCircle(SCRWIDTH/2, osc.xEP, 10, BLACK); //oscillator
+        DrawLineEx((Vector2){SCRWIDTH/2, SCRHEIGHT/2}, (Vector2){SCRWIDTH/2 + simState.A * cos(sqrt(simState.K / simState.m) * simState.t), osc.xEP}, 5.0, BLACK); //trig circle
+        DrawDottedLines(SCRWIDTH/2, osc.xEP, (SCRWIDTH/2 + simState.A * cos(sqrt(simState.K / simState.m) * simState.t)), osc.xEP, BLACK);
+
+
+    EndDrawing();
+}
+
+physicsVals OscilatorSimulation(float A, float K, float m, char *surfaceType){
+    float omega = sqrt(K/m);
+    if (surfaceType == "V"){
+        float d = m*10/K;
+    }
+    float xEP = SCRHEIGHT/2 - (A * sin(omega * simState.t));
+    return (physicsVals){xEP, xEP, xEP, xEP, xEP, xEP, xEP};
+}
 
 
 
@@ -87,7 +87,7 @@ void DrawDottedLines(int xi, int yi, int xf, int yf, Color color){
         float currentX = xi;
         float currentY = yi;
         float dx = 10; float dy = dx*L;
-        while (currentX + dx < xf && currentY + dy <= yf){
+        while (currentX + dx < abs(xf) && currentY + dy <= yf){
             DrawLineEx((Vector2){currentX, currentY}, (Vector2){currentX + dx, currentY + dy}, 5.0, color);
             currentX += dx * 2;
             currentY += dy * 2;
