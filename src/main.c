0ac3841685d94ea6aa960fcb2eddc928093666ee
panos <panos99sk@gmail.com>
2023-10-31 01:07:45 +0200

    start on gui

--- src/main.c
+++ src/main.c
@@ -2,8 +2,8 @@
 #include "raymath.h"
 #include "stdlib.h"
 #include "stdio.h"
-#include "util.h"
 #include "math.h"
+#include "raygui.h"
 
 #define SCRWIDTH 1200
 #define SCRHEIGHT 800
@@ -24,7 +24,7 @@ physicsVals OscilatorSimulation(float A, float K, float m, char *surfaceType);
 void draw();
 
 
-state simState = (state){A: 300, K: 10, m: 3, t: 0, paused: false, surfaceType: "V"}; //A edw = A(m) * 100, gia na fenetai
+state* simState = (state*){/*A*/300, /*K*/10, /*m*/ 3,/*t*/ 0, /*Paused*/ false, /*surfaceType*/ "V"}; //A edw = A(m) * 100, gia na fenetai
 
 int main(void)
 {   // initialization
@@ -38,7 +38,7 @@ int main(void)
     {
         //updating variables
         if(!paused){
-            simState.t += GetFrameTime();
+            simState->t += GetFrameTime();
             draw();    
         }    
 
@@ -52,33 +52,33 @@ void draw(){
     //draw
     BeginDrawing();
         //statics
-        DrawRing((Vector2){SCRWIDTH/2, SCRHEIGHT/2}, simState.A, simState.A + 5, 0, 360, 1, RED);
-        DrawDottedLines(SCRWIDTH/2 - simState.A, SCRHEIGHT/2, SCRWIDTH/2 + simState.A, SCRHEIGHT/2, RED);
-        DrawDottedLines(SCRWIDTH/2, SCRHEIGHT/2 + simState.A, SCRWIDTH/2, SCRHEIGHT/2 - simState.A, BLUE);
-        DrawText("Equilibrium position", SCRWIDTH/2 + simState.A + 10, SCRHEIGHT/2 - 9, 18, RED);
-        DrawDottedLines(SCRWIDTH/2 - simState.A, SCRHEIGHT/2 - (simState.m * 10 / simState.K) * 100, SCRWIDTH/2 + simState.A, SCRHEIGHT/2 - (simState.m * 10 / simState.K) * 100, GREEN); //natural length pos
-        DrawText("Natural Length position", SCRWIDTH/2 + simState.A + 10, SCRHEIGHT/2 - (simState.m * 10 / simState.K) * 100 - 9, 18, GREEN);
+        DrawRing((Vector2){SCRWIDTH/2, SCRHEIGHT/2}, simState->A, simState->A + 5, 0, 360, 1, RED);
+        DrawDottedLines(SCRWIDTH/2 - simState->A, SCRHEIGHT/2, SCRWIDTH/2 + simState->A, SCRHEIGHT/2, RED);
+        DrawDottedLines(SCRWIDTH/2, SCRHEIGHT/2 + simState->A, SCRWIDTH/2, SCRHEIGHT/2 - simState->A, BLUE);
+        DrawText("Equilibrium position", SCRWIDTH/2 + simState->A + 10, SCRHEIGHT/2 - 9, 18, RED);
+        DrawDottedLines(SCRWIDTH/2 - simState->A, SCRHEIGHT/2 - (simState->m * 10 / simState->K) * 100, SCRWIDTH/2 + simState->A, SCRHEIGHT/2 - (simState->m * 10 / simState->K) * 100, GREEN); //natural length pos
+        DrawText("Natural Length position", SCRWIDTH/2 + simState->A + 10, SCRHEIGHT/2 - (simState->m * 10 / simState->K) * 100 - 9, 18, GREEN);
         ClearBackground(RAYWHITE);
 
         char* staticVariables;
-        asprintf(&staticVariables, "A: %f m \nK: %f N/m \nm: %f kg \nt: %f s \nPaused: %i", simState.A/100, simState.K, simState.m, simState.t, simState.paused);
+        asprintf(&staticVariables, "A: %f m \nK: %f N/m \nm: %f kg \nt: %f s \nPaused: %i", simState->A/100, simState->K, simState->m, simState->t, simState->paused);
         DrawText(staticVariables, 5, 24, 18, BLACK);
         free(staticVariables);
 
         //dynamics
         DrawFPS(5, 0);
-        physicsVals osc = OscilatorSimulation(simState.A, simState.K, simState.m, "V");
+        physicsVals osc = OscilatorSimulation(simState->A, simState->K, simState->m, "V");
         DrawCircle(SCRWIDTH/2, SCRHEIGHT/2 - osc.xEP, 10, BLACK); //oscillator
-        DrawLineEx((Vector2){SCRWIDTH/2, SCRHEIGHT/2}, (Vector2){SCRWIDTH/2 + simState.A * cos(sqrt(simState.K / simState.m) * simState.t), SCRHEIGHT/2 - osc.xEP}, 5.0, BLACK); //trig circle
-        if(cos(sqrt(simState.K / simState.m) * simState.t)>0){
-            DrawDottedLines(SCRWIDTH/2, SCRHEIGHT/2 - osc.xEP, (SCRWIDTH/2 + simState.A * cos(sqrt(simState.K / simState.m) * simState.t)), SCRHEIGHT/2 - osc.xEP, BLACK);
+        DrawLineEx((Vector2){SCRWIDTH/2, SCRHEIGHT/2}, (Vector2){SCRWIDTH/2 + simState->A * cos(sqrt(simState->K / simState->m) * simState->t), SCRHEIGHT/2 - osc.xEP}, 5.0, BLACK); //trig circle
+        if(cos(sqrt(simState->K / simState->m) * simState->t)>0){
+            DrawDottedLines(SCRWIDTH/2, SCRHEIGHT/2 - osc.xEP, (SCRWIDTH/2 + simState->A * cos(sqrt(simState->K / simState->m) * simState->t)), SCRHEIGHT/2 - osc.xEP, BLACK);
         }
-        else if(cos(sqrt(simState.K / simState.m) * simState.t)<0){
-            DrawDottedLines(SCRWIDTH/2 + simState.A * cos(sqrt(simState.K / simState.m) * simState.t), SCRHEIGHT/2 - osc.xEP, SCRWIDTH/2, SCRHEIGHT/2 - osc.xEP, BLACK);
+        else if(cos(sqrt(simState->K / simState->m) * simState->t)<0){
+            DrawDottedLines(SCRWIDTH/2 + simState->A * cos(sqrt(simState->K / simState->m) * simState->t), SCRHEIGHT/2 - osc.xEP, SCRWIDTH/2, SCRHEIGHT/2 - osc.xEP, BLACK);
         }
         
         //!DEBUG
-        if(simState.t < 4){
+        if(simState->t < 4){
             printf("xEP: %f m \nxNL: %f m\nu: %f m/s \na: %f m/s^2 \nTF: %f N\nKE: %f J\nUE: %f J \nTE: %f J\n", osc.xEP, osc.xNL, osc.u, osc.a, osc.TF, osc.KE, osc.UE, osc.TE);
         }
 
@@ -86,6 +86,13 @@ void draw(){
         asprintf(&variableValues, "xEP: %f m \nxNL: %f m\nu: %f m/s \na: %f m/s^2 \nTF: %f N\nKE: %f J\nUE: %f J \nTE: %f J", osc.xEP, osc.xNL, osc.u, osc.a, osc.TF, osc.KE, osc.UE, osc.TE);
         DrawText(variableValues, 5, SCRHEIGHT - 18 * 8 * 3/2, 18, BLACK);
         free(variableValues);
+
+
+        //GUI
+        GuiSliderBar((Rectangle){}, "A:", NULL, &simState->A, 0, 400);
+        GuiSliderBar((Rectangle){}, "K:", NULL, &simState->A, 0, 500);
+        GuiSliderBar((Rectangle){}, "m:", NULL, &simState->A, 0, 500);
+        
 
 
     EndDrawing();
@@ -97,10 +104,10 @@ physicsVals OscilatorSimulation(float A, float K, float m, char *surfaceType){
     if (surfaceType == "V"){
         d = m*10/K;
     }
-    float xEP = (A * sin(omega * simState.t));
+    float xEP = (A * sin(omega * simState->t));
     float xNL = (d + xEP);
-    float u = omega * A * cos(omega * simState.t);
-    float a = -omega * omega * A * sin(omega * simState.t);
+    float u = omega * A * cos(omega * simState->t);
+    float a = -omega * omega * A * sin(omega * simState->t);
     float TF = -K * xEP;
     float KE = 1/2 * m * u*u;
     float UE = 1/2 * K * xEP*xEP;
