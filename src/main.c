b82350d62b9ee5c11304c03d984512428d128499
panos <panos99sk@gmail.com>
2023-10-31 13:38:35 +0200

    COMPLETED PROJECT

--- src/main.c
+++ src/main.c
@@ -20,30 +20,26 @@ typedef struct{
     bool paused;
     char* surfaceType;
 }state;
+
+state simStateInit = (state){A:300, K: 10, m: 3,t: 0, paused: false, surfaceType: "V"}; //A edw = A(m) * 100, gia na fenetai
+state* simState = &simStateInit;
+
 //extra functions
 void DrawDottedLines(int xi, int yi, int xf, int yf, Color color);
 physicsVals OscilatorSimulation(float A, float K, float m, char *surfaceType);
 void draw();
 
-
-state simStateSt = (state){A:300, K: 10, m: 3,t: 0, paused: false, surfaceType: "V"}; //A edw = A(m) * 100, gia na fenetai
-state* simState = &simStateSt;
-
 int main(void)
 {   // initialization
     InitWindow(SCRWIDTH, SCRHEIGHT, "Simple harmonic oscillator simulation by Panagiotis Skoulis");
     SetTargetFPS(FPS); //sets framerate
-    bool paused = false;
 
     //init
 
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         //updating variables
-        if(!paused){
-            simState->t += GetFrameTime();
-            draw();    
-        }    
+        draw();  
 
     }
 
@@ -54,6 +50,10 @@ int main(void)
 void draw(){
     //draw
     BeginDrawing();
+
+        if(!simState->paused){
+            simState->t += GetFrameTime();
+        }  
         //statics
         DrawRing((Vector2){SCRWIDTH/2, SCRHEIGHT/2}, simState->A, simState->A + 5, 0, 360, 1, RED);
         DrawDottedLines(SCRWIDTH/2 - simState->A, SCRHEIGHT/2, SCRWIDTH/2 + simState->A, SCRHEIGHT/2, RED);
@@ -63,10 +63,10 @@ void draw(){
         DrawText("Natural Length position", SCRWIDTH/2 + simState->A + 10, SCRHEIGHT/2 - (simState->m * 10 / simState->K) * 100 - 9, 18, GREEN);
         ClearBackground(RAYWHITE);
 
-        char* staticVariables;
-        asprintf(&staticVariables, "A: %f m \nK: %f N/m \nm: %f kg \nt: %f s \nPaused: %i", simState->A/100, simState->K, simState->m, simState->t, simState->paused);
+        char staticVariables[200];
+        snprintf(staticVariables, sizeof(staticVariables),"A: %f m \nK: %f N/m \nm: %f kg \nt: %f s \nPaused: %i", simState->A/100, simState->K, simState->m, simState->t, simState->paused);
         DrawText(staticVariables, 5, 24, 18, BLACK);
-        free(staticVariables);
+        //free(staticVariables);
 
         //dynamics
         DrawFPS(5, 0);
@@ -80,23 +80,16 @@ void draw(){
             DrawDottedLines(SCRWIDTH/2 + simState->A * cos(sqrt(simState->K / simState->m) * simState->t), SCRHEIGHT/2 - osc.xEP, SCRWIDTH/2, SCRHEIGHT/2 - osc.xEP, BLACK);
         }
         
-        //!DEBUG
-        if(simState->t < 4){
-            printf("xEP: %f m \nxNL: %f m\nu: %f m/s \na: %f m/s^2 \nTF: %f N\nKE: %f J\nUE: %f J \nTE: %f J\n", osc.xEP, osc.xNL, osc.u, osc.a, osc.TF, osc.KE, osc.UE, osc.TE);
-        }
 
-        char* variableValues;
-        asprintf(&variableValues, "xEP: %f m \nxNL: %f m\nu: %f m/s \na: %f m/s^2 \nTF: %f N\nKE: %f J\nUE: %f J \nTE: %f J", osc.xEP, osc.xNL, osc.u, osc.a, osc.TF, osc.KE, osc.UE, osc.TE);
+        char variableValues[200];
+        snprintf(variableValues, sizeof(variableValues), "xEP: %f m \nxNL: %f m\nu: %f m/s \na: %f m/s^2 \nTF: %f N\nKE: %f J\nUE: %f J \nTE: %f J", osc.xEP, osc.xNL, osc.u, osc.a, osc.TF, osc.KE, osc.UE, osc.TE);
         DrawText(variableValues, 5, SCRHEIGHT - 18 * 8 * 3/2, 18, BLACK);
-        free(variableValues);
-
 
         //GUI
         GuiSliderBar((Rectangle){SCRWIDTH - 210, 10, 200, 40}, "A:", NULL, &simState->A, 0, 400);
         GuiSliderBar((Rectangle){SCRWIDTH - 210, 60, 200, 40}, "K:", NULL, &simState->K, 0, 100);
         GuiSliderBar((Rectangle){SCRWIDTH - 210, 110, 200, 40}, "m:", NULL, &simState->m, 0, 100);
-        
-
+        GuiCheckBox((Rectangle){SCRWIDTH - 210, 160, 40, 40}, "Paused:", &simState->paused);
 
     EndDrawing();
 }
@@ -112,9 +105,9 @@ physicsVals OscilatorSimulation(float A, float K, float m, char *surfaceType){
     float u = omega * A * cos(omega * simState->t);
     float a = -omega * omega * A * sin(omega * simState->t);
     float TF = -K * xEP;
-    float KE = 1/2 * m * u*u;
-    float UE = 1/2 * K * xEP*xEP;
-    float TE = 1/2 * K * A*A;
+    float KE = (m * u*u)/2;
+    float UE = (K * xEP*xEP)/2;
+    float TE = (K * A*A/100)/2;
 
     return (physicsVals){xEP: xEP,xNL: xNL,u: u,a: a,TF: TF,KE: KE,UE: UE,TE: TE};
 }
